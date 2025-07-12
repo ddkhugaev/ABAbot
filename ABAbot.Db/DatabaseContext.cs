10908861b21c8f05692f7fa168fe3fa789c28818
David Khugaev <ddkhugaev@gmail.com>
2025-07-12 16:12:36 +0300

    Сделана база данных

---
+++ ABAbot.Db/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using ABAbot.Db.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABAbot.Db
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ikigai> Ikigaies { get; set; }
+        
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
